08e93019d4f168e26d9fc2f813a48bdd81155bd0
Herick Carneiro <herickcarneiro@gmail.com>
2026-03-12 16:07:06 -0300

    refactor: ajusta models de categoria

--- back-end/app/models/categoria.py
+++ back-end/app/models/categoria.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Integer, String, Boolean
+import uuid
+from sqlalchemy import Column, Integer, String, Boolean, UUID
 from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Categoria(Base):
     __tablename__ = "categoria"
 
-    id_categoria = Column(Integer, primary_key=True, index=True)
+    id_categoria = Column(UUID(as_uuid = True), primary_key= True, default=uuid.uuid4)
     nome_categoria = Column(String, nullable=False, unique=True)
 
     produtos = relationship("Produto", back_populates="categoria")
